75b3edebf220337f51c0733462e09c708c6c2a9a
Artyom Gutsulyak <38570098+Imweyboss@users.noreply.github.com>
2022-03-09 09:57:57 +0300

    Update deliveryPipe.groovy

--- vars/deliveryPipe.groovy
+++ vars/deliveryPipe.groovy
@@ -1,4 +1,8 @@
-def call(Map pipelineParams){
+def call(body){
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
     
     pipeline {
         agent any
