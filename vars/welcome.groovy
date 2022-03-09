ca509e327b90fe349afb0c4e93ddd2e9f8f75def
Artyom Gutsulyak <38570098+Imweyboss@users.noreply.github.com>
2022-03-09 09:44:06 +0300

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,3 +1,14 @@
-def call(name){
-  echo "Hey ${name}------"
+def call(text){
+    def pipelineParams = [:]
+    text.resolveStrategy = Closure.DELEGATE_FIRST
+    text.delegate = pipelineParams
+    text()
+  pipeline {
+      agent any
+    stage('TEST!'){
+      steps {
+        echo 'TSSSSSSSSSSSS'
+      }
+    }  
+  }
 }
